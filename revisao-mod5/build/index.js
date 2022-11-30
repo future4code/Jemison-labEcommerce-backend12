2d886e521a43f5ed1abfab49a1337d332d4fa1f7
rafix923 <rafaellopes.dev@gmail.com>
2022-11-29 22:16:57 -0300

    Se adicionar esse commit, incluirá a revisão de Introdução ao Typescript.

---
+++ revisao-mod5/build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const chalk_1 = require("chalk");
+const message = chalk_1.default.red("Hello World!");
+console.log(message);
+//# sourceMappingURL=index.js.map
--- revisao-mod5/index.js
+++
@@ -1,4 +0,0 @@
-import  chalk  from "chalk";
-
-const message = chalk.red("Hello World!")
-console.log(message);
