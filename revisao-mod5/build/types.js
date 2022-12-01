12f58ce2fb5ead5e6bbce396bf0a937a98af1621
rafix923 <rafaellopes.dev@gmail.com>
2022-12-01 08:19:02 -0300

    commit

---
+++ revisao-mod5/build/types.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GENDER = void 0;
+var GENDER;
+(function (GENDER) {
+    GENDER["MALE"] = "male";
+    GENDER["FEMALE"] = "female";
+    GENDER["OTHER"] = "other";
+})(GENDER = exports.GENDER || (exports.GENDER = {}));
+;
