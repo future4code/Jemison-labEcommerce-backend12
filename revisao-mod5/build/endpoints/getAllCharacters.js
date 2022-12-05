8d5e4bd32000d3d313ab100d314967c4bd2b701c
rafix923 <rafaellopes.dev@gmail.com>
2022-12-05 14:50:19 -0300

    fix: Se adicionar esse commit, irá mover as pastas build e node_modules para a raiz do projeto. Resolverá a falha com o deploy do projeto pelo render

--- revisao-mod5/build/endpoints/getAllCharacters.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const data_1 = require("../data");
-function default_1(req, res) {
-    res.send(data_1.characters);
-}
-exports.default = default_1;
